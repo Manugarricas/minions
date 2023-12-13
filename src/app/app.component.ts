1c109fb22ce96ec57913eedf1f0ca25386341afa
Manuel <manugarricas@gmail.com>
2023-12-13 09:38:38 +0100

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { InlineComponent } from './inline/inline.component';
+import { MinionsComponent } from './minions/minions.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, InlineComponent, MinionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/inline/inline.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inline',
+  standalone: true,
+  imports: [],
+  templateUrl: './inline.component.html'
+})
+export class InlineComponent {
+
+}
